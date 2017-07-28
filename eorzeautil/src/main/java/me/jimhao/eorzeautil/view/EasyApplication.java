58f9a22edf21478c172dc09f83e361faab995312
顾浩然 <806869597@qq.com>
2017-07-28 17:16:06 +0800

    为EasyApplication增加全局异常捕获机制

--- eorzeautil/src/main/java/me/jimhao/eorzeautil/view/EasyApplication.java
+++ eorzeautil/src/main/java/me/jimhao/eorzeautil/view/EasyApplication.java
@@ -6,6 +6,8 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 
+import me.jimhao.eorzeautil.log.CrashHandlerUtil;
+
 /**
  * 作者： guhaoran
  * 创建于： 2017/7/26
@@ -15,13 +17,16 @@ import android.content.pm.PackageManager;
 public abstract class EasyApplication extends Application {
     public static boolean isDebug = true ;  //默认为true
     public static String appName = null ;
-
+    private Context context ;
     @Override
     public void onCreate() {
         super.onCreate();
+        context = getApplicationContext();
         isDebug = getIsDebug();   //查看是否为debug开发模式
         appName = getAppName(); //获得App名称
-        onBusiness(getApplicationContext());   //完成业务操作
+
+        registerCrashHandler(context);
+        onBusiness(context);   //完成业务操作
     }
 
     private boolean getIsDebug(){
@@ -42,4 +47,9 @@ public abstract class EasyApplication extends Application {
     }
 
     public abstract void onBusiness(Context context);
+
+    public void registerCrashHandler(Context context){
+        CrashHandlerUtil util = CrashHandlerUtil.getInstance();
+        util.init(context);
+    }
 }
